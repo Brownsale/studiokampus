1eba33fa2a0b66a796c72a56d22f5603a3edf09f
Brownsale <Bruno@lanf.ca>
2022-04-20 19:09:16 -0400

    GA4 test

--- pages/_app.js
+++ pages/_app.js
@@ -8,20 +8,21 @@ import "@/css/theme.css";
 import "@/css/responsive.css";
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
-import * as ga from '../lib/ga'
+import * as gtag from '../lib/gtag'
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
-
   useEffect(() => {
     const handleRouteChange = (url) => {
-      ga.pageview(url)
+      gtag.pageview(url)
     }
     router.events.on('routeChangeComplete', handleRouteChange)
     return () => {
       router.events.off('routeChangeComplete', handleRouteChange)
     }
   }, [router.events])
+  
   return <Component {...pageProps} />;
 }
+
 export default MyApp;
