742cc74d69f2894369019c7ad7bf70b66ebbbb09
Brunsalle <bruno@lanf.ca>
2024-01-10 15:16:54 -0500

    test

--- components/google-map.js
+++ components/google-map.js
@@ -4,12 +4,10 @@ const GoogleMap = ({ extraClass }) => {
   return (
     <div className={`google-map__${extraClass}`}>
       <iframe
-
         title="template google map"
         src="https://www.google.com/maps/d/u/0/embed?mid=1lJkmvkAoFk8RT4_9Olyd7R2t7BjsuqA&ehbc=2E312F"
         className={`map__${extraClass}`}
         allowFullScreen
-        
       ></iframe>
     </div>
   );
