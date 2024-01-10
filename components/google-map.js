5adfe3d4a09610066348f40e5e86aad99675c0d8
Brunsalle <bruno@lanf.ca>
2024-01-10 15:30:03 -0500

    test test

--- components/google-map.js
+++ components/google-map.js
@@ -3,14 +3,12 @@ import React from "react";
 const GoogleMap = ({ extraClass }) => {
   return (
     <div className={`google-map__${extraClass}`}>
-
       <iframe
         title="template google map"
         src="https://www.google.com/maps/d/u/0/embed?mid=1lJkmvkAoFk8RT4_9Olyd7R2t7BjsuqA&ehbc=2E312F"
         className={`map__${extraClass}`}
         allowFullScreen
       ></iframe>
-      
     </div>
   );
 };
